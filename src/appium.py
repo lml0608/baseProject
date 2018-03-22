5ca3bb45a45a7699d02b412e231653dfe200c4d7
liubin <liumeile0608@163.com>
2018-03-22 17:18:57 +0800

    12

--- src/appium.py
+++ src/appium.py
@@ -4,6 +4,10 @@ __author__:liubin
 
 '''
 import logging
+import time
+import os
+
+import subprocess
 
 class appium(object):
 
@@ -16,5 +20,37 @@ class appium(object):
     def start(self):
 
         self.logger.info('starting Appium Server...')
+
+        currentTime = time.strftime('%Y%m%d%H%M%S',time.localtime())
+
+        udid          = ''
+        appiumPort    = 1
+        bootStrapPort = 2
+        seldnroidPort = 3
+        chromiumPort  = 4
+
+        logPath       = os.path.abspath(os.path.join(os.getcwd(),'log','AS'+currentTime+'.log'))
+
+
+        self.process = subprocess.Popen('appium --port={} --bootstrap-port={} --seldnroid-port={} --chromedriver-port={} '
+                                        '--log={} -U{}'.format(appiumPort,bootStrapPort,seldnroidPort,chromiumPort,logPath,udid),stdout=subprocess.PIPE,shell=True)
+
+
+
+        time.sleep(5)
+
+
+    def getUdid(self):
+
+        cmd = 'adb devices'
+
+        data = subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True)
+
+
+
+
+
+
+
 
 
